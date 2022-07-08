6c63a0f831cbcb1c3730eb560a7c5e10247a4f5e
Lucas Ribeiro <lucasribeirodesouza@live.com>
2022-07-07 22:43:48 -0300

    Projeto finalizado

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var toolSelector = document.getElementById('tooltip')
+var tooltip = new bootstrap.Tooltip(toolSelector)
+
+var myCarousel = document.querySelector('#carouselCidades')
+var carousel = new bootstrap.Carousel(myCarousel, { interval: 10 })
